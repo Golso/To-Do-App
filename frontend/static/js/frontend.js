09514d9ea76634bcb8745572cecdf63da4909aca
Golso <matq666@gmail.com>
2020-08-07 12:51:43 +0200

    Updating task.

---
+++ frontend/static/js/frontend.js
@@ -0,0 +1,91 @@
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+const csrftoken = getCookie('csrftoken');
+
+let activeItem = null
+
+buildList()
+
+function buildList(){
+    let wrapper = document.getElementById('list-wrapper')
+    wrapper.innerHTML = ''
+    let url = 'http://127.0.0.1:8000/api/task-list/'
+    fetch(url)
+    .then((resp) => resp.json())
+    .then(function(data){
+        console.log('Data:', data)
+
+        let list = data
+        for (let i in list){
+
+            let item = `
+                <div id="data-row-${i}" class="task-wrapper flex-wrapper">
+                    <div style="flex:7">
+                        <span class="title">${list[i].title}</span>
+                    </div>
+                    <div style="flex:1">
+                        <button class="btn btn-sm btn-outline-info edit">Edit</button>
+                    </div>
+                    <div style="flex:1">
+                        <button class="btn btn-sm btn-outline-dark delete">-</button>
+                    </div>
+                </div>
+            `
+            wrapper.innerHTML += item
+        }
+
+        for (let i in list){
+            let editBtn = document.getElementsByClassName('edit')[i]
+
+            editBtn.addEventListener('click', (function(item){
+                return function(){
+                    editItem(item)
+                }
+            })(list[i]))
+        }
+    })
+}
+
+let form = document.getElementById('form-wrapper')
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+    console.log('Form submitted')
+    let url = 'http://127.0.0.1:8000/api/task-create/'
+    if (activeItem != null){
+        url = `http://127.0.0.1:8000/api/task-update/${activeItem.id}/`
+        activeItem = null
+    }
+    let title = document.getElementById('title').value
+    fetch(url, {
+        method:'POST',
+        headers:{
+            'Content-type':'application/json',
+            'X-CSRFToken': csrftoken,
+        },
+        body:JSON.stringify({'title': title})
+    }
+    ).then(function(response){
+        buildList()
+        document.getElementById('form').reset()
+    })
+})
+
+function editItem(item){
+    console.log('Item clicked:', item)
+    activeItem = item
+    document.getElementById('title').value = activeItem.title
+}
